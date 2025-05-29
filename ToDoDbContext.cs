42425bdfad7be805a5393ae5f3e931ea0db623ee
Noa-gv <noagv1765@gmail.com>
2025-05-29 21:28:53 +0300

    change the dbcontexs

--- ToDoDbContext.cs
+++ ToDoDbContext.cs
@@ -1,58 +1,73 @@
 ﻿using System;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
-namespace TodoApi;
-
-public partial class ToDoDbContext : DbContext
+namespace TodoApi
 {
-    public ToDoDbContext()
+    public partial class ToDoDbContext : DbContext
     {
-    }
-
-    public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
-        : base(options)
-    {
-    }
-
-    public virtual DbSet<Item> Items { get; set; }
-    public virtual DbSet<User> Users { get; set; }  // הוספנו את DbSet עבור המשתמשים
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseMySql("name=ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder
-            .UseCollation("utf8mb4_0900_ai_ci")
-            .HasCharSet("utf8mb4");
-
-        modelBuilder.Entity<Item>(entity =>
+        public ToDoDbContext()
         {
-            entity.HasKey(e => e.Id).HasName("PRIMARY");
+        }
 
-            entity.ToTable("items");
-
-            entity.Property(e => e.Name).HasMaxLength(100);
-        });
-
-        // הגדרת הטבלה של Users
-        modelBuilder.Entity<User>(entity =>
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+            : base(options)
         {
-            entity.HasKey(e => e.Id).HasName("PRIMARY");
+        }
 
-            entity.ToTable("users");
+        public virtual DbSet<Item> Items { get; set; }
+        public virtual DbSet<User> Users { get; set; }
 
-            entity.Property(e => e.Username)
-                .HasMaxLength(50)
-                .IsRequired(); // שם משתמש חובה
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true)
+                    .AddEnvironmentVariables()
+                    .Build();
 
-            entity.Property(e => e.PasswordHash)
-                .HasMaxLength(255) // סיסמה בפורמט hash
-                .IsRequired(); // סיסמה חובה
-        });
+                var connectionString = config.GetConnectionString("DefaultConnection");
 
-        OnModelCreatingPartial(modelBuilder);
+                optionsBuilder.UseMySql(connectionString, ServerVersion.Parse("8.0.40-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .UseCollation("utf8mb4_0900_ai_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<Item>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+                entity.ToTable("items");
+
+                entity.Property(e => e.Name).HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+                entity.ToTable("users");
+
+                entity.Property(e => e.Username)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                entity.Property(e => e.PasswordHash)
+                    .HasMaxLength(255)
+                    .IsRequired();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
